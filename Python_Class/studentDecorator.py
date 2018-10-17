3212ebced9384c208ed39edec4240318e748abc2
zaoad <zaoadcsedu22.23@gmail.com>
2018-10-17 15:44:24 +0600

    commit

--- Python_Class/studentDecorator.py
+++ Python_Class/studentDecorator.py
@@ -1,3 +1,25 @@
+# Python 3.4+
+from abc import ABC, abstractmethod
+from Python_Class.ComponentPerson import ComponentPerson
+class Abstract(ComponentPerson,ABC):
+    @abstractmethod
+    def foo(self):
+        pass
 
+# # Python 3.0+
+# from abc import ABCMeta, abstractmethod
+# class Abstract(metaclass=ABCMeta):
+#     @abstractmethod
+#     def foo(self):
+#         pass
+#
+# # Python 2
+# from abc import ABCMeta, abstractmethod
+# class Abstract:
+#     __metaclass__ = ABCMeta
+#
+#     @abstractmethod
+#     def foo(self):
+#         pass
 
 
